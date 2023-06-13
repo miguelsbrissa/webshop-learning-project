d6020cb12089e0cc30d46951070f8bfb5de90995
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-13 18:03:53 -0300

    feat: :sparkles: Adds funcionality to home page
    Now it's possible to add items to the cart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { Product } from "src/app/models/product.model";
+import { CartService } from "src/app/services/cart.service";
 
 const ROWS_HEIGH: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
@@ -10,11 +12,23 @@ export class HomeComponent {
   cols = 3;
   rowHeight = ROWS_HEIGH[this.cols];
   category: string | undefined;
+
+  constructor(private cartService: CartService) {}
+
   onColumnsCountChanged(colsNumber: number) {
     this.cols = colsNumber;
     this.rowHeight = ROWS_HEIGH[this.cols];
   }
   onShowCategory(newCategory: string) {
     this.category = newCategory;
+  }
+  onAddToCart(product: Product) {
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.priice,
+      quantity: 1,
+      id: product.id,
+    });
   }
 }
