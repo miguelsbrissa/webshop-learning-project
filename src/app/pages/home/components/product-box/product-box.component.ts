4c22360903c741810aab9150714be74362c3fcf8
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-13 17:45:02 -0300

    feat: :sparkles: Adds AddToCart method in product-box component

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Product } from "src/app/models/product.module";
 
 @Component({
-  selector: 'app-product-box',
-  templateUrl: './product-box.component.html'
+  selector: "app-product-box",
+  templateUrl: "./product-box.component.html",
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
-
-  
+  @Output() addToCart = new EventEmitter();
+  product: Product | undefined = {
+    id: 1,
+    title: "Snickers",
+    priice: 150,
+    category: "shoes",
+    description: "Description",
+    image: "https://via.placeholder.com/150",
+  };
+  onAddToCart() {
+    this.addToCart.emit(this.product);
+  }
 }
