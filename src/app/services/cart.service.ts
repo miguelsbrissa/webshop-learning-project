4aa90e2698beab9966114694f5b516aa7157d5ba
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-13 18:51:06 -0300

    feat: :sparkles: Adds removeQuantity method and update the removeFromCart method in cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -32,12 +32,38 @@ export class CartService {
     this.cart.next({ items: [] });
     this._snackBar.open("Cart is clear.", "Ok", { duration: 3000 });
   }
-  removeFromCart(item: CartItem) {
+  removeFromCart(item: CartItem, update = true): Array<CartItem> {
     const filteredItems = this.cart.value.items.filter(
       (_item) => _item.id !== item.id
-    )
+    );
 
-    this.cart.next({items: filteredItems})
-    this._snackBar.open("Item removed from the Cart.", "Ok", { duration: 3000 });
+    if (update) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open("Item removed from the Cart.", "Ok", {
+        duration: 3000,
+      });
+    }
+    return filteredItems;
+  }
+  removeQuantity(item: CartItem) {
+    let itemForRemoval: CartItem | undefined;
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity--;
+
+        if (_item.quantity === 0) {
+          itemForRemoval = _item;
+        }
+      }
+      return _item;
+    });
+
+    if (itemForRemoval) {
+      filteredItems = this.removeFromCart(itemForRemoval, false);
+    }
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open("1 item removed from the Cart", "Ok", {
+      duration: 3000,
+    });
   }
 }
