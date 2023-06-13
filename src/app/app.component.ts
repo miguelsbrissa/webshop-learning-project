80a3a11baa8455e44b8b4691cf14012d31217177
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-13 18:25:59 -0300

    feat: :sparkles: Adds cart service to App and add cart to app-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Cart } from "./models/cart.model";
+import { CartService } from "./services/cart.service";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <app-header></app-header>
+    <app-header [cart]="cart"></app-header>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [],
 })
-export class AppComponent {
-  title = 'webshop-learning-project';
+export class AppComponent implements OnInit {
+  cart: Cart = { items: [] };
+
+  constructor(private cartService: CartService) {}
+  ngOnInit() {
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart;
+    });
+  }
 }
