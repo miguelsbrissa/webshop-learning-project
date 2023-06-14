f761e8be618fbfb5375f2a3db0e74e52703270be
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-14 12:42:57 -0300

    feat: :sparkles: Adds Outputs to limit and sort

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -5,16 +5,22 @@ import { Component, Output, EventEmitter } from "@angular/core";
   templateUrl: "./products-header.component.html",
 })
 export class ProductsHeaderComponent {
-  @Output() columnsCountChange = new EventEmitter<number>()
+  @Output() columnsCountChange = new EventEmitter<number>();
+  @Output() sortChange = new EventEmitter<string>();
+  @Output() itemsCountChange = new EventEmitter<number>();
+
   sort = "desc";
   itemsShowCount = 12;
+
   onSortUpdated(newSort: string) {
     this.sort = newSort;
+    this.sortChange.emit(newSort);
   }
   onItemsUpdated(count: number) {
     this.itemsShowCount = count;
+    this.itemsCountChange.emit(count);
   }
-  onColumnsUpdated(colsNumber: number){
-    this.columnsCountChange.emit(colsNumber)
+  onColumnsUpdated(colsNumber: number) {
+    this.columnsCountChange.emit(colsNumber);
   }
 }
