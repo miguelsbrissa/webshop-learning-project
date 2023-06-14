82cf98320ad79840328b31f9f3eb8f6963e1a0c9
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-14 12:33:27 -0300

    feat: :sparkles: Makes product-box dynamic

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -8,14 +8,8 @@ import { Product } from "src/app/models/product.model";
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
   @Output() addToCart = new EventEmitter();
-  product: Product | undefined = {
-    id: 1,
-    title: "Snickers",
-    priice: 150,
-    category: "shoes",
-    description: "Description",
-    image: "https://via.placeholder.com/150",
-  };
+  @Input() product: Product | undefined;
+  
   onAddToCart() {
     this.addToCart.emit(this.product);
   }
