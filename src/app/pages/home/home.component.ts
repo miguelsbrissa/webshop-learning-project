b113b8c1949e9d9abc7521f6ccf062339b6716af
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-06-14 12:57:27 -0300

    feat: :sparkles: Makes home page dynamic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { Product } from "src/app/models/product.model";
 import { CartService } from "src/app/services/cart.service";
+import { StoreService } from "src/app/services/store.service";
 
 const ROWS_HEIGH: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
@@ -8,12 +10,31 @@ const ROWS_HEIGH: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
   selector: "app-home",
   templateUrl: "./home.component.html",
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   cols = 3;
   rowHeight = ROWS_HEIGH[this.cols];
   category: string | undefined;
+  products: Array<Product> | undefined;
+  sort = "desc";
+  count = 12;
+  productsSubscription: Subscription | undefined;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private storeService: StoreService
+  ) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productsSubscription = this.storeService
+      .getAllProducts(this.count, this.sort, this.category)
+      .subscribe((_products) => {
+        this.products = _products;
+      });
+  }
 
   onColumnsCountChanged(colsNumber: number) {
     this.cols = colsNumber;
@@ -21,14 +42,28 @@ export class HomeComponent {
   }
   onShowCategory(newCategory: string) {
     this.category = newCategory;
+    this.getProducts();
   }
   onAddToCart(product: Product) {
     this.cartService.addToCart({
       product: product.image,
       name: product.title,
-      price: product.priice,
+      price: product.price,
       quantity: 1,
       id: product.id,
     });
+  }
+  onItemsCountChange(newCount: number) {
+    this.count = newCount;
+    this.getProducts();
+  }
+  onSortChange(newSort: string) {
+    this.sort = newSort;
+    this.getProducts();
+  }
+  ngOnDestroy() {
+    if (this.productsSubscription) {
+      this.productsSubscription.unsubscribe();
+    }
   }
 }
